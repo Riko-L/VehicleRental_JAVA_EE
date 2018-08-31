688a26ac555d86f78de6445390ae91a401d6072e
Riko-L <ledevedec.eric@gmail.com>
2018-08-31 15:25:26 +0200

    app

--- src/com/campusnumerique/vehiclerental/utils/Constante.java
+++ src/com/campusnumerique/vehiclerental/utils/Constante.java
@@ -6,7 +6,7 @@ public class Constante {
 	public final static int AGE_MOINS_DE_18 = 0;
 	public final static int AGE_MOINS_DE_21 = 8;
 	public final static int AGE_ENTRE_21_ET_25 = 13;
-	public final static int AGE_PLUS_25 = -1;
+	public final static int AGE_PLUS_25 = Integer.MAX_VALUE;
 
 	
 }
