13ec91718250b066cb556a57f00cb4d5c72d850e
Riko-L <ledevedec.eric@gmail.com>
2018-09-03 13:07:28 +0200

    Debut itération 3

---
+++ src/com/campusnumerique/vehiclerental/dao/MotorBikeDAO.java
@@ -0,0 +1,42 @@
+package com.campusnumerique.vehiclerental.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.campusnumerique.vehiclerental.entity.MotorBike;
+
+public class MotorBikeDAO extends DAO<MotorBike> {
+
+	@Override
+	public boolean create(MotorBike obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(MotorBike obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(MotorBike obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public MotorBike find(int id) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<MotorBike> findAll() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+}
---
+++ src/com/campusnumerique/vehiclerental/dao/UtilityCarDAO.java
@@ -0,0 +1,40 @@
+package com.campusnumerique.vehiclerental.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.campusnumerique.vehiclerental.entity.UtilityCar;
+
+public class UtilityCarDAO extends DAO<UtilityCar> {
+
+	@Override
+	public boolean create(UtilityCar obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(UtilityCar obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(UtilityCar obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public UtilityCar find(int id) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<UtilityCar> findAll() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
